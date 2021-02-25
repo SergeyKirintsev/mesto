c2e62ef3fa0ce99ad45d310bac9b37dd63b8e8d3
SergeyKirintsev <kserg80@gmail.com>
2021-02-25 21:47:34 +0300

    подготовка validate.js

---
+++ scripts/validate.js
@@ -0,0 +1,8 @@
+enableValidation({
+  formSelector: ".popup__form",
+  inputSelector: ".popup__input",
+  submitButtonSelector: ".popup__form-submit",
+  inactiveButtonClass: "popup__form-submit_disabled",
+  inputErrorClass: "popup__input-error",
+  errorClass: "popup__input-error_active",
+});
