3ad391fa5604f85bc3c80c8394818ade4900f8ae
SergeyKirintsev <kserg80@gmail.com>
2021-03-27 23:12:58 +0300

    создать класс Section

---
+++ src/components/section.js
@@ -0,0 +1,22 @@
+export default class Section {
+  constructor({ items, renderer }, containerSelector) {
+    this._renderedItems = items;
+    this._renderer = renderer;
+    this._container = document.querySelector(containerSelector);
+  }
+
+  renderItems() {
+    this.clear();
+    this._renderedItems.forEach((item) => {
+      this._renderer(item);
+    });
+  }
+
+  clear() {
+    this._container.innerHTML = '';
+  }
+
+  addItem(element) {
+    this._container.prepend(element);
+  }
+}
