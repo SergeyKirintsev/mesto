3aa35f9f01353e8258afd7012b26effa21ad5958
SergeyKirintsev <kserg80@gmail.com>
2021-03-27 23:56:20 +0300

    create class PopupWithForm

---
+++ src/components/PopupWithForm.js
@@ -0,0 +1,15 @@
+import Popup from "./Popup";
+
+export class PopupWithForm extends Popup {
+  constructor() {
+    super();
+  }
+
+  _getInputValues() {}
+
+  setEventListeners() {}
+
+  close() {
+    super.close();
+  }
+}
